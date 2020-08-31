ff51b042f8fa8d82f051de5ebfbe2883d149fa8f
Katrin Dav <k.dawidowicz2016@gmail.com>
2020-08-31 18:13:07 +0200

    init commit - basic structure of project

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <section className="main">
+            <h1>Kandinsky.</h1>
+            <div className="row">
+                <h2>
+                    <div className="line">
+                        <span>Color is a power which</span>
+                    </div>
+                    <div className="line">
+                        <span>directly infuences the soul</span>
+                    </div>
+                </h2>
+                <h3><span>&#8210;</span>Wassily Kandinsky</h3>
+            </div>
+
+
+        </section>
+    );
+}
+
+export default Header;
---
+++ src/components/Themes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Themes = () => {
+    return (
+        <section className="container">
+            <div className="row-item about">
+                <h2>
+                    abstrakcjonizm <br />
+                    w sztuce
+                </h2>
+            </div>
+            <div className="row-item bio">
+                <h2>
+                    Kandinsky <br />
+                    <span>&#8210;</span>krótkie bio
+                </h2>
+            </div>
+            <div className="row-item art">
+                <h2>
+                    Kandinsky <br />
+                    <span>&#8210;</span>malarstwo
+                </h2>
+            </div>
+        </section>
+    );
+}
+
+export default Themes;
