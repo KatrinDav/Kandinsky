baca3a01ce399c8d1f05b72c74d0b36eca5580ed
Katrin Dav <k.dawidowicz2016@gmail.com>
2020-09-14 19:48:56 +0200

    create table of pictures

--- src/pages/Arts.js
+++ src/pages/Arts.js
@@ -1,5 +1,37 @@
 import React from 'react';
 
+const artsTab = [
+    { id: 1, src: '../img/1908.jpg', title: 'Murnau Street With Women, 1908' },
+    { id: 2, src: '../img/1909.jpg', title: 'Weilheim - Marienplatz, 1909' },
+    { id: 3, src: '../img/lastSupper.jpg', title: 'Last Supper, 1909 - 1910' },
+    { id: 4, src: '../img/1910.jpg', title: 'Untitled First Abstract Watercolor, 1910' },
+    { id: 5, src: '../img/kompozycja_5.jpg', title: 'Composition V, 1911' },
+    { id: 6, src: '../img/1912.jpg', title: 'The Last Judgment, 1912' },
+    { id: 7, src: '../img/kwadraty.jpg', title: 'Squares with Concentric Circles, 1913' },
+    { id: 8, src: '../img/kompozycja_7.jpg', title: 'Composition VII, 1913' },
+    { id: 9, src: '../img/zolte.jpg', title: 'Little Painting with Yellow, 1914' },
+    { id: 10, src: '../img/george.jpg', title: 'Rider St.George, 1915-1916' },
+    { id: 11, src: '../img/watercolor.jpg', title: 'Watercolor for Poul Bjerre, 1916' },
+    { id: 12, src: '../img/Imatra.jpg', tile: 'Imatra, 1917' },
+    { id: 13, src: '../img/39.jpg', title: 'In Grey, 1919' },
+    { id: 14, src: '../img/draft.jpg', title: 'Draft for Mural In The Unjuried Art Show, 1922' },
+    { id: 15, src: '../img/black.jpg', title: 'Two Movements, 1924' },
+    { id: 16, src: '../img/aglow.jpg', title: 'Inner Alliance, 1929' },
+    { id: 17, src: '../img/diagonal.jpg', title: 'Diagonal, 1930' },
+    { id: 18, src: '../img/fun.jpg', title: 'Serious Fun, 1930' },
+    { id: 19, src: '../img/1.jpg', title: 'Gravitation, 1935' },
+    { id: 20, src: '../img/comp9.jpg', title: 'Composition IX, 1936' },
+    { id: 21, src: '../img/domin.jpg', title: 'Dominant Curve, 1936' },
+    { id: 22, src: '../img/thirty.jpg', title: 'Thirty, 1937' },
+    { id: 23, src: '../img/open.jpg', title: 'The Opening, 1938' },
+    { id: 24, src: '../img/kompozycja_x.jpg', title: 'Composition X, 1939' },
+    { id: 25, src: '../img/around.jpg', title: 'Around the Circle, 1940' },
+    { id: 26, src: '../img/little.jpg', title: 'Little Accents, 1940' },
+    { id: 27, src: '../img/four.jpg', title: 'Four Figures on Three Squares, 1943' },
+    { id: 28, src: '../img/last.jpg', title: 'Last Watercolour, 1944' },
+
+];
+
 const Arts = () => {
     return (
         <>
