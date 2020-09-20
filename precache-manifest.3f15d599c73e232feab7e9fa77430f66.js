c82273534218978fedfd75a07ba678d4516245ed
Katrin Dav <k.dawidowicz2016@gmail.com>
2020-09-20 12:04:18 +0200

    Updates

---
+++ precache-manifest.3f15d599c73e232feab7e9fa77430f66.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62106d91fd2767bf42ce4330c2a47ab7",
+    "url": "/kandinsky/index.html"
+  },
+  {
+    "revision": "efe6c416f398d6b5d757",
+    "url": "/kandinsky/static/css/main.fdf3bcb6.chunk.css"
+  },
+  {
+    "revision": "183109899416d0de231b",
+    "url": "/kandinsky/static/js/2.2c0b8102.chunk.js"
+  },
+  {
+    "revision": "85c1ecd325742a4c0551d2653970c6fb",
+    "url": "/kandinsky/static/js/2.2c0b8102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe6c416f398d6b5d757",
+    "url": "/kandinsky/static/js/main.4d2b41d7.chunk.js"
+  },
+  {
+    "revision": "5d6645f6260f464326ef",
+    "url": "/kandinsky/static/js/runtime-main.dc402863.js"
+  },
+  {
+    "revision": "ae36306c10f957ec3d1af52886a4e1df",
+    "url": "/kandinsky/static/media/1.ae36306c.jpg"
+  },
+  {
+    "revision": "4f26162014a30e3d81ed523cf4a55e29",
+    "url": "/kandinsky/static/media/1908.4f261620.jpg"
+  },
+  {
+    "revision": "f630440614b3a84a241f62a9cb6a49e9",
+    "url": "/kandinsky/static/media/1909.f6304406.jpg"
+  },
+  {
+    "revision": "0ba6bdf0e3dcccd543a8f2b9b1c8840c",
+    "url": "/kandinsky/static/media/1910.0ba6bdf0.jpg"
+  },
+  {
+    "revision": "a1f23c6f840fe3e178711ce1750ffd7e",
+    "url": "/kandinsky/static/media/1912.a1f23c6f.jpg"
+  },
+  {
+    "revision": "bb2dd50ef683707384a3e49d64ac731d",
+    "url": "/kandinsky/static/media/39.bb2dd50e.jpg"
+  },
+  {
+    "revision": "01288d49f098f0f098a43c5838d5f378",
+    "url": "/kandinsky/static/media/Imatra.01288d49.jpg"
+  },
+  {
+    "revision": "8d4081ad803203eec4b31cd955adf376",
+    "url": "/kandinsky/static/media/abstrakcja-2.8d4081ad.jpg"
+  },
+  {
+    "revision": "15f4aef5cda11970dba7de5bd762f13d",
+    "url": "/kandinsky/static/media/aglow.15f4aef5.jpg"
+  },
+  {
+    "revision": "2913f765f725cfcf8bbd7fb601631396",
+    "url": "/kandinsky/static/media/around.2913f765.jpg"
+  },
+  {
+    "revision": "2a36528ba89a2e13ca03317e93b6219c",
+    "url": "/kandinsky/static/media/black-red.2a36528b.jpg"
+  },
+  {
+    "revision": "e419c8d60ac71f7e2b24334739fed2a1",
+    "url": "/kandinsky/static/media/black.e419c8d6.jpg"
+  },
+  {
+    "revision": "09ae337ff42b4ec4515cc5f2664b4fce",
+    "url": "/kandinsky/static/media/comp9.09ae337f.jpg"
+  },
+  {
+    "revision": "8894e893580847819bb0ac5c68b8ef3d",
+    "url": "/kandinsky/static/media/diagonal.8894e893.jpg"
+  },
+  {
+    "revision": "2c471f4359f9fec73d9e5910f3ed69ad",
+    "url": "/kandinsky/static/media/domin.2c471f43.jpg"
+  },
+  {
+    "revision": "e49a0b7ff91a638231c16aa4d4f4afb0",
+    "url": "/kandinsky/static/media/draft.e49a0b7f.jpg"
+  },
+  {
+    "revision": "90f4ede12f25453774f5f491c0465f78",
+    "url": "/kandinsky/static/media/fixed.90f4ede1.jpg"
+  },
+  {
+    "revision": "21539613ac6f6c72b1f2f226187508eb",
+    "url": "/kandinsky/static/media/four.21539613.jpg"
+  },
+  {
+    "revision": "9e90be2d0dcfb20e9e7f48b3d8feae5c",
+    "url": "/kandinsky/static/media/fun.9e90be2d.jpg"
+  },
+  {
+    "revision": "567e3df30693b62784fd1b03d5908db9",
+    "url": "/kandinsky/static/media/george.567e3df3.jpg"
+  },
+  {
+    "revision": "549794c66d3725101cc5e12017fa3402",
+    "url": "/kandinsky/static/media/green.549794c6.jpg"
+  },
+  {
+    "revision": "94e4357f4b7056bc59ca6c8903fe25ad",
+    "url": "/kandinsky/static/media/kandinsky.94e4357f.jpg"
+  },
+  {
+    "revision": "9bf5744e6e36fa7114d6e13ac37be10b",
+    "url": "/kandinsky/static/media/kandinsky_color.9bf5744e.jpg"
+  },
+  {
+    "revision": "3c5e232633c11fd05e1837075ac731c5",
+    "url": "/kandinsky/static/media/kompozycja_5.3c5e2326.jpg"
+  },
+  {
+    "revision": "4812dc4dcdf2b5706448c2808ed85c9c",
+    "url": "/kandinsky/static/media/kompozycja_7.4812dc4d.jpg"
+  },
+  {
+    "revision": "3af57c602fdf58b5a49f013d293e669e",
+    "url": "/kandinsky/static/media/kompozycja_x.3af57c60.jpg"
+  },
+  {
+    "revision": "97a308d977890f3fdf31b4383fdb84e3",
+    "url": "/kandinsky/static/media/kwadraty.97a308d9.jpg"
+  },
+  {
+    "revision": "be462a47686cd61e3aa93be2e0282b59",
+    "url": "/kandinsky/static/media/last.be462a47.jpg"
+  },
+  {
+    "revision": "d535a97b22c69a43eb2e727fd3039178",
+    "url": "/kandinsky/static/media/lastSupper.d535a97b.jpg"
+  },
+  {
+    "revision": "2aeb70769abdcbd86d239cfac33565fe",
+    "url": "/kandinsky/static/media/little.2aeb7076.jpg"
+  },
+  {
+    "revision": "06b921c5b586a154b3b3a05074428302",
+    "url": "/kandinsky/static/media/muses.06b921c5.jpg"
+  },
+  {
+    "revision": "31916d83fefb4718d1eee40c42389d33",
+    "url": "/kandinsky/static/media/open.31916d83.jpg"
+  },
+  {
+    "revision": "69a41dcc726aa46da0f24b74ca760545",
+    "url": "/kandinsky/static/media/striped.69a41dcc.jpg"
+  },
+  {
+    "revision": "eed9bc3c6f79c41aaa93c16413698878",
+    "url": "/kandinsky/static/media/thirty.eed9bc3c.jpg"
+  },
+  {
+    "revision": "4e04a24b8e725920636ebde0adb46690",
+    "url": "/kandinsky/static/media/watercolor.4e04a24b.jpg"
+  },
+  {
+    "revision": "f93165333e76c96b5a288b1c0e1444f6",
+    "url": "/kandinsky/static/media/zolte.f9316533.jpg"
+  }
+]);
